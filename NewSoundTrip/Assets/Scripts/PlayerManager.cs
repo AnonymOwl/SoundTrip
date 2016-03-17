fc0572b4c45fbf8843d5a11031445bced6e9acc7
Severine Febvet <severinefebvet@gmail.com>
2016-03-17 13:27:12 +0900

    First project commit with a base game

---
+++ NewSoundTrip/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public enum 			Side {Left, Right};
+
+	public Side				playerSide;
+
+	private Camera			leftCamera;
+	private Camera			rightCamera;
+	private GameObject		rightPlayer;
+	private PointsCreator	center;
+
+	private Vector3			mousePosition;
+	private Vector3			offset;
+
+	void Start () {
+		leftCamera = GameObject.Find("LeftCamera").GetComponent<Camera>();
+		rightCamera = GameObject.Find("RightCamera").GetComponent<Camera>();
+		rightPlayer = GameObject.Find("RightPlayer");
+		center = GameObject.Find("Center").GetComponent<PointsCreator>();
+	}
+	
+	void Update () {
+		if (playerSide == Side.Right) {
+			if (Application.platform == RuntimePlatform.Android)
+				mousePosition = Input.GetTouch(0).position;
+			else
+				mousePosition = Input.mousePosition;
+			mousePosition = rightCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 10));
+			transform.position = mousePosition;
+		}
+		if (playerSide == Side.Left) {
+			transform.position = new Vector3(-rightPlayer.transform.position.x, rightPlayer.transform.position.y, 0);
+			if (Vector3.Distance(transform.position, Vector3.zero) > center.distance) {
+				offset = transform.position;
+				offset.Normalize();
+				transform.position = offset * center.distance;
+			}
+		}
+	}
+}
---
+++ NewSoundTrip/Assets/Scripts/PointsCreator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointsCreator : MonoBehaviour {
+
+	public int			distance = 1;
+	public GameObject	parent;
+	private float		cosAngle;
+	private float		sinAngle;
+
+	void Start () {
+		cosAngle = Mathf.Cos(Mathf.Deg2Rad * 45);
+		sinAngle = Mathf.Sin(Mathf.Deg2Rad * 45);
+
+		Transform p0 = parent.transform.FindChild("Point0");
+		p0.position = new Vector3(0, distance, 0);
+		Transform p1 = parent.transform.FindChild("Point1");
+		p1.position = new Vector3(cosAngle * distance, sinAngle * distance, 0);
+		Transform p2 = parent.transform.FindChild("Point2");
+		p2.position = new Vector3(distance, 0, 0);
+		Transform p3 = parent.transform.FindChild("Point3");
+		p3.position = new Vector3(cosAngle * distance, -sinAngle * distance, 0);
+		Transform p4 = parent.transform.FindChild("Point4");
+		p4.position = new Vector3(0, -distance, 0);
+		Transform p5 = parent.transform.FindChild("Point5");
+		p5.position = new Vector3(-cosAngle * distance, -sinAngle * distance, 0);
+		Transform p6 = parent.transform.FindChild("Point6");
+		p6.position = new Vector3(-distance, 0, 0);
+		Transform p7 = parent.transform.FindChild("Point7");
+		p7.position = new Vector3(-cosAngle * distance, sinAngle * distance, 0);
+
+		GameObject.Find("Tunnel").transform.localScale = new Vector3(distance * 2.5f, distance * 2.5f, 1);
+	}
+}
