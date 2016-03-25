25b628fc69898947f15db75b1109ef29ffbc980b
Severine Febvet <severinefebvet@gmail.com>
2016-03-26 01:31:55 +0900

    first part of item timer: writes times into file

---
+++ NewSoundTrip/Assets/Scripts/ItemTimer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+//TODO: put that in the thing that actually places the elements, you only need key codes here
+[System.Serializable]
+public class ItemValues {
+	public KeyCode		code;
+	public GameObject	prefab;
+}
+
+public class ItemTimer : MonoBehaviour {
+
+	public bool				isOn;
+	public AudioSource		source;
+	public ItemValues[]		values;
+	public string			filename;
+
+	private string			input;
+	private StreamWriter	sw;
+
+	void Start () {
+		if (isOn) {
+			if (File.Exists("Assets/SongFiles/" + filename))
+				File.Delete("Assets/SongFiles/" + filename);
+			sw = File.CreateText("Assets/SongFiles/" + filename);
+		}
+	}
+	
+	void Update () {
+		if (isOn) {
+			input = Input.inputString;
+			if (input != "") {
+				foreach (ItemValues v in values) {
+					if (input.ToLower()[0] == v.code.ToString().ToLower()[0]) {
+						sw.WriteLine(source.time + ":" + v.code.ToString());
+					}
+				}
+			}
+		}
+	}
+
+	void OnApplicationQuit() {
+		if (isOn)
+			sw.Close();
+	}
+		
+}
---
+++ NewSoundTrip/Assets/Scripts/MoveManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveManager : MonoBehaviour {
+
+	public int			speed;
+
+	private AudioSource	music;
+	private bool		musicOn = false;
+
+	void Start () {
+		music = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		if (transform.position.z <= 0 && !musicOn) {
+			music.Play();
+			musicOn = true;
+		}
+		if (!musicOn)
+			transform.Translate(0, 0, -speed * Time.deltaTime);
+		else {
+			transform.position = new Vector3(0, 0, -music.time * speed);
+		}
+	}
+}
