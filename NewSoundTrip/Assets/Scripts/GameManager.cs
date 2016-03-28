ced7ba0700d0f2e66f5d54700df987afb5403d22
Severine Febvet <severinefebvet@gmail.com>
2016-03-28 15:46:56 +0900

    orientation fix because i'm not smart

--- NewSoundTrip/Assets/Scripts/GameManager.cs
+++ NewSoundTrip/Assets/Scripts/GameManager.cs
@@ -12,15 +12,17 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	void Update () {
-		if (vertical == false && Screen.orientation == ScreenOrientation.Portrait) {
-			leftCamera.rect = new Rect(0f, 0.5f, 1f, 0.5f);
-			rightCamera.rect = new Rect(0f, 0f, 1f, 0.5f);
-			vertical = true;
-		}
-		else if (vertical == true && Screen.orientation == ScreenOrientation.Landscape) {
-			leftCamera.rect = new Rect(0f, 0f, 0.5f, 1f);
-			rightCamera.rect = new Rect(0.5f, 0f, 0.5f, 1f);
-			vertical = false;
+		if (Application.platform == RuntimePlatform.Android) {
+			if (vertical == false && Screen.orientation == ScreenOrientation.Portrait) {
+				leftCamera.rect = new Rect(0f, 0.5f, 1f, 0.5f);
+				rightCamera.rect = new Rect(0f, 0f, 1f, 0.5f);
+				vertical = true;
+			}
+			else if (vertical == true && Screen.orientation == ScreenOrientation.Landscape) {
+				leftCamera.rect = new Rect(0f, 0f, 0.5f, 1f);
+				rightCamera.rect = new Rect(0.5f, 0f, 0.5f, 1f);
+				vertical = false;
+			}
 		}
 	}
 }
